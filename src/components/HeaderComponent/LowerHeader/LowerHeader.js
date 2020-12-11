9e02dfdada0ec79406b4e8eef9a319035a8b22e6
Andrey Kharebov <andreykharebov@gmail.com>
2020-12-11 19:16:57 +0300

    Added header adaptive css afunctionality

--- src/components/HeaderComponent/LowerHeader/LowerHeader.js
+++ src/components/HeaderComponent/LowerHeader/LowerHeader.js
@@ -4,58 +4,76 @@ import classes from "./LowerHeader.module.css";
 
 function LowerHeader() {
   const [activeNavBar, setActiveNavBar] = useState(false);
+  const [mobileMenuActive, setMobileMenuActive] = useState(false);
 
   const changeBG = () => {
-    if (window.scrollY >= 100) {
-      setActiveNavBar(true);
-    } else {
-      setActiveNavBar(false);
+    if (window.innerWidth >= 982) {
+      if (window.scrollY >= 100) {
+        setActiveNavBar(true);
+      } else {
+        setActiveNavBar(false);
+      }
     }
   };
 
   window.addEventListener("scroll", changeBG);
 
+  const mobileMenuHandler = () => {
+    setMobileMenuActive(a => !a);
+  }
+
   return (
-    <div
-      className={
-        activeNavBar
-          ? `${classes.lowerHeader} ${classes.blacked}`
-          : classes.lowerHeader
-      }
-    >
+    <div className={  activeNavBar ? `${classes.lowerHeader} ${classes.blacked}` : classes.lowerHeader }>
       <div className={classes.headerLogo}>
         <img src="./assets/logo.png" alt="logo"></img>
       </div>
-      <div className={classes.headerMenu}>
+      <div className={ classes.burgerMenu } onClick={ mobileMenuHandler }>
+        <i className="fas fa-bars"></i>
+      </div>
+      <div className={  mobileMenuActive ? `${classes.headerMenu} ${classes.activeMobileMenu}` : classes.headerMenu }>
         <ul>
-          <li>
             <Link
              to="about"
              offset={-100}
              smooth={true}
-            >about us</Link>
-          </li>
-          <li><Link
+             onClick={ mobileMenuHandler }
+            ><li>about us</li></Link>
+          
+        
+            <Link
              to="packages"
              smooth={true}
-            >packages</Link></li>
-          <li><Link
+             onClick={ mobileMenuHandler }
+            ><li>packages</li></Link>
+          
+        
+            <Link
              to="videos"
              smooth={true}
-            >videos</Link></li>
-          <li><Link
+             onClick={ mobileMenuHandler }
+            ><li>videos</li></Link>
+          
+        
+            <Link
              to="store"
              smooth={true}
-            >store</Link></li>
-          <li><Link
+             onClick={ mobileMenuHandler }
+            ><li>store</li></Link>
+          
+        
+            <Link
              to="patreon"
              smooth={true}
-            >patreon</Link></li>
-          <li><Link
+             onClick={ mobileMenuHandler }
+            ><li>patreon</li></Link>
+          
+            <Link
              to="contact"
              offset={-100}
              smooth={true}
-            >contact</Link></li>
+             onClick={ mobileMenuHandler }
+            ><li>contact</li></Link>
+          
         </ul>
       </div>
     </div>
